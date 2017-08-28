49c94cb09242e07837daef9bb60602de32d34bf9
root <root@ssd42.dbg.private>
2017-08-28 20:50:37 +0800

    4

--- DNNmodel.py
+++ DNNmodel.py
@@ -24,8 +24,8 @@ def DNN(x):
 
   # First convolutional layer - maps one grayscale image to 10 feature maps.
   with tf.name_scope('conv1'):
-    W_conv1 = weight_variable([5, 5, 1, 10])
-    b_conv1 = bias_variable([10])
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
   # Pooling layer - downsamples by 2X.
@@ -34,8 +34,8 @@ def DNN(x):
 
   # Second convolutional layer -- maps 10 feature maps to 20.
   with tf.name_scope('conv2'):
-    W_conv2 = weight_variable([5, 5, 10, 20])
-    b_conv2 = bias_variable([20])
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
   # Second pooling layer.
@@ -45,28 +45,21 @@ def DNN(x):
   # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
   # is down to 7x7x20 feature maps -- maps this to 400 features.
   with tf.name_scope('fc1'):
-    W_fc1 = weight_variable([7 * 7 * 20, 400])
-    b_fc1 = bias_variable([400])
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*20])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
   
-  #maps -- maps 400 to 400 features
-  with tf.name_scope('fc2'):
-    W_fc2 = weight_variable([400, 400])
-    b_fc2 = bias_variable([400])
-
-    h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
-
   # Dropout - controls the complexity of the model, prevents co-adaptation of
   # features.
   with tf.name_scope('dropout'):
     keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc2, keep_prob)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
   # Map the 400 features to 10 classes, one for each digit
   with tf.name_scope('softmax'):
-    W_fc2 = weight_variable([400, 10])
+    W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
 
     y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
