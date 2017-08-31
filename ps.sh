5d919cd343e891bb69701bef9d56535777bbefe2
AndyShen <shenhang105@gmail.com>
2017-08-31 15:14:56 +0800

    7

--- ps.sh
+++ ps.sh
@@ -25,6 +25,13 @@ get_worker_conf(){
     worker=${worker:1}
 };
 
+for(( i=0; i<$2; i++ ))
+do
+{
+    echo "0">temp$i
+}
+done
+
 echo "release port occpied!"
 ./kill_cluster_pid.sh 26 35 $5
 
@@ -35,31 +42,46 @@ echo $worker
 
 #rm -rf ./result/$1"-"$2
 #mkdir ./result/$1"-"$2
- 
+flag_arr[0]=0
 for(( i=35; i>35-$1-$2; i-- ))
 do
 {
     if [ $i == 35 ]
     then
-        source /root/anaconda2/envs/tensorflow/bin/activate
-        python /root/DMLcode/disDNN.py $ps $worker --job_name=ps --task_index=0
+	source /root/anaconda2/envs/tensorflow/bin/activate
+        python /root/code/disDNN/disDNN.py $ps $worker --job_name=ps --task_index=0
     else
 	ssh ssd$i "source activate tensorflow"
         n=`expr 35 - $1`
         if [ $i -gt $n ]
         then
             index=`expr 35 - $i`
-            ssh ssd$i python /root/DMLcode/disDNN.py $ps $worker --job_name=ps --task_index=$index
+            ssh ssd$i python /root/code/disDNN/disDNN.py $ps $worker --job_name=ps --task_index=$index
         else
             index=`expr 35 - $1 - $i`
-            ssh ssd$i python /root/DMLcode/disDNN.py $ps $worker --job_name=worker --task_index=$index --targted_accuracy=$4 --optimizer=$3 #>> /root/DMLcode/result/$6
+	    ssh ssd$i python /root/code/disDNN/disDNN.py $ps $worker --job_name=worker --task_index=$index --targted_accuracy=$4 --optimizer=$3 #>> /root/DMLcode/result/$6
             echo "worker"$index" complated"
+	    echo "1">temp$index
 	fi
     fi
 }&
+done
+flag=0
+while true
+do
+    for(( i=0; i<$2; i++ ))
+    do
+    {   
+	tem=`cat temp$i`
+	flag=`expr $tem + $flag`
+	echo $flag
+    }
+    done	
+    if [ $flag == $2 ]
+    then
+    	./kill_cluster_pid.sh 26 35 $5
+	break
+    fi
 done 
-wait
-echo "done"
-
-
-
+rm -f temp*
+echo "work done"
---
+++ start_experiment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "ps=1 workers=*"
+./ps.sh 1 1 Adam 0.9 2223
+./ps.sh 1 2 Adam 0.9 2223
+./ps.sh 1 3 Adam 0.9 2223
+./ps.sh 1 4 Adam 0.9 2223
+./ps.sh 1 5 Adam 0.9 2223
+./ps.sh 1 6 Adam 0.9 2223
+./ps.sh 1 7 Adam 0.9 2223
+./ps.sh 1 8 Adam 0.9 2223
+./ps.sh 1 9 Adam 0.9 2223
